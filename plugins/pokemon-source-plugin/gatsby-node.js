ecae4a900dfb2154aa075c4feddf2b2932fda477
Claudia Afonso <claudia1087@gmail.com>
2023-03-30 22:36:06 +0200

    adds nodes with data

--- plugins/pokemon-source-plugin/gatsby-node.js
+++ plugins/pokemon-source-plugin/gatsby-node.js
@@ -1,44 +1,142 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
-// You can delete this file if you're not using it
+// ○ Pokemon number - DONE
+// ○ Pokemon name (in correct language) - DONE
+// ○ Pokemon genus (in correct language) -
+// ○ Pokemon description (in correct language) - DONE
+// ○ Pokemon image -
 
-/**
- * You can uncomment the following line to verify that
- * your plugin is being loaded in your site.
- *
- * See: https://www.gatsbyjs.com/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
- */
-// exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+const fetch = require('node-fetch')
 
-// constants for your GraphQL Post and Author types
-const POST_NODE_TYPE = `Post`
+exports.sourceNodes = async ({ actions: { createNode }, createContentDigest }) => {
+  const promises = []
+  const images = []
+  const urlSpecies = 'https://pokeapi.co/api/v2/pokemon-species/'
+  const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/'
 
-exports.sourceNodes = async ({ actions, createContentDigest, createNodeId }) => {
-  const { createNode } = actions
-
-  const data = {
-    posts: [
-      { id: 1, description: `Hello world!` },
-      { id: 2, description: `Second post!` },
-    ],
+  for (let i = 1; i <= 151; i++) {
+    promises.push(fetch(`${urlSpecies}/${i}`).then((res) => res.json()))
+    images.push(fetch(`${urlPokemon}/${i}`).then((res) => res.json()))
   }
 
-  // loop through data and create Gatsby nodes
-  data.posts.forEach((post) =>
-    createNode({
-      ...post,
-      id: createNodeId(`${POST_NODE_TYPE}-${post.id}`),
-      parent: null,
-      children: [],
-      internal: {
-        type: POST_NODE_TYPE,
-        contentDigest: createContentDigest(post),
-      },
-    })
-  )
+  const getPokemonImage = (sprites) => {
+    let results = sprites
+      .filter((el) => el.language.name === 'it' || el.language.name === 'es' || el.language.name === 'en')
+      .map((filteredData) => {
+        const language = {}
+        const key = filteredData.language.name
+        language[key] = filteredData.genus
 
-  return
+        return {
+          language,
+        }
+      })
+
+    return results
+  }
+
+  const getPokemonGenus = (genera) => {
+    let results = genera
+      .filter((el) => el.language.name === 'it' || el.language.name === 'es' || el.language.name === 'en')
+      .map((filteredData) => {
+        const language = {}
+        const key = filteredData.language.name
+        language[key] = filteredData.genus
+
+        return language
+      })
+      .reduce((acc, lang) => {
+        return { ...acc, ...lang }
+      })
+
+    return results
+  }
+
+  const getPokemonDescriptionAndVersion = (descriptions) => {
+    let resultsX = descriptions
+      .filter((el) => el.version.name === 'x')
+      .filter(
+        (versions) =>
+          versions.language.name === 'it' || versions.language.name === 'es' || versions.language.name === 'en'
+      )
+      .map((filteredData) => {
+        const language = {}
+        const key = filteredData.language.name
+        language[key] = filteredData.flavor_text
+
+        return language
+      })
+      .reduce((acc, lang) => {
+        return { ...acc, ...lang }
+      })
+
+    let resultsY = descriptions
+      .filter((el) => el.version.name === 'y')
+      .filter(
+        (versions) =>
+          versions.language.name === 'it' || versions.language.name === 'es' || versions.language.name === 'en'
+      )
+      .map((filteredData) => {
+        const language = {}
+        const key = filteredData.language.name
+        language[key] = filteredData.flavor_text
+
+        return language
+      })
+      .reduce((acc, lang) => {
+        return { ...acc, ...lang }
+      })
+
+    return { x: resultsX, y: resultsY }
+  }
+
+  const getPokemonName = (names) => {
+    let results = names
+      .filter((lang) => lang.language.name === 'it' || lang.language.name === 'es' || lang.language.name === 'en')
+      .map((filteredData) => {
+        const language = {}
+        const key = filteredData.language.name
+        language[key] = filteredData.name
+
+        return language
+      })
+      .reduce((acc, lang) => {
+        return { ...acc, ...lang }
+      })
+
+    return results
+  }
+
+  const transformData = (pokemon) => {
+    const name = getPokemonName(pokemon.names)
+    const details = getPokemonDescriptionAndVersion(pokemon.flavor_text_entries)
+    const genus = getPokemonGenus(pokemon.genera)
+
+    // console.log(name)
+
+    const pokemonData = {
+      name,
+      genus,
+      details,
+      // image: result.sprites.other.home.front_default,
+      id: pokemon.id,
+    }
+
+    return pokemonData
+  }
+
+  Promise.all(promises).then((results) => {
+    const pokemonList = results.map((result) => {
+      return transformData(result)
+    })
+
+    pokemonList.forEach((item) => {
+      createNode({
+        ...item,
+        id: 'pokemon-' + item.id,
+        internal: {
+          type: 'Pokemon',
+          contentDigest: createContentDigest(item),
+        },
+      })
+    })
+  })
 }
