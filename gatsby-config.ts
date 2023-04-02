e8ef0e0b841cfdfe4a1567ac53fa61f464fcbbec
Claudia Afonso <claudia1087@gmail.com>
2023-04-02 20:35:40 +0200

    adds missing file from previous commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,39 +1,62 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `pokédex`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Pokédex challenge`,
+    siteUrl: `https://www.yourdomain.tld`,
+    description: `Pokédex - Web team take-home challenge`,
+    image: `/src/images/pokeball.png`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", "pokemon-source-plugin", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/favicon.png",
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sitemap',
+    'pokemon-source-plugin',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/favicon.png'
+      }
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    'gatsby-plugin-mdx',
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          formats: [`auto`, `webp`],
+          placeholder: `dominantColor`,
+          quality: 50,
+          backgroundColor: `transparent`
+        }
+      }
     },
-    __key: "pages"
-  }, {
-    resolve: `gatsby-plugin-sass`,
-    options: {
-      additionalData: `@import "./src/styles/main.scss";`,
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/'
+      },
+      __key: 'images'
     },
-  }, {
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/'
+      },
+      __key: 'pages'
+    },
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        additionalData: `@import "./src/styles/main.scss";`
+      }
+    },
+    {
       resolve: `gatsby-omni-font-loader`,
       options: {
         enableListener: true,
@@ -41,18 +64,20 @@ const config: GatsbyConfig = {
         web: [
           {
             name: `Open Sans`,
-            file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`,
-          },
-        ],
-      },
-    }, {
+            file: `https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap`
+          }
+        ]
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/locales`,
         name: `locale`
       }
-    }, {
-    resolve: `gatsby-plugin-react-i18next`,
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
       options: {
         localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
         languages: [`en`, `es`, `it`],
@@ -75,13 +100,9 @@ const config: GatsbyConfig = {
           }
         ]
       }
-  },{
-    resolve: `gatsby-plugin-remote-images`,
-    options: {
-      nodeType: 'Pokemon',
-      imagePath: 'nodes[].imageUrl'
     },
-  }, "gatsby-plugin-image", "gatsby-awesome-pagination"]
-};
+    'gatsby-plugin-image'
+  ]
+}
 
-export default config;
+export default config
